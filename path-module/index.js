253f6c1d26dbc0581c8f11b93beb33422ab6dc7c
Uttam <uttamrohit4545@gmail.com>
2025-07-25 22:48:40 +0600

    feat: use path module to resolve file and directory paths

--- path-module/index.js
+++ path-module/index.js
@@ -1,7 +1,12 @@
-import path from "path";
+import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
-console.log(__filename);
+
+const __dirname = path.dirname(__filename)
+
+const fullPath = path.join(__dirname, 'example' ,' output.txt')
+
+console.log("Full path :" , fullPath)
 
 console.log("Basename:", path.basename("/foo/bar/baz/asdf/index.html"))
