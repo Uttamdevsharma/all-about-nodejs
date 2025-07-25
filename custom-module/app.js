728e809adcbae7843ad9456ffc6ef4355c7f339b
Uttam <uttamrohit4545@gmail.com>
2025-07-26 02:40:28 +0600

    feat : custom-module create

---
+++ custom-module/app.js
@@ -0,0 +1,6 @@
+
+const subtraction = (a,b) => a - b;
+const multiply = (a,b) => a * b;
+
+const mathmaticalOperations = {subtraction,multiply};
+export default mathmaticalOperations;
---
+++ custom-module/main.js
@@ -0,0 +1,8 @@
+import { addition } from "./math.js";
+import mathmaticalOperations from "./app.js";
+
+const additionRes = addition(2,3);
+console.log("Addition is : " , additionRes);
+
+const res = mathmaticalOperations.subtraction(20,10);
+console.log(res);
