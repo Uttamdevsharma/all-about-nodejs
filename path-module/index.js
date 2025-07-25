d7261e4bc5c6084f71256049026541e253de93da
Uttam <uttamrohit4545@gmail.com>
2025-07-25 22:41:58 +0600

    feat : full path url using fileURLPath

--- path-module/index.js
+++ path-module/index.js
@@ -1,3 +1,7 @@
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+console.log(__filename);
 
 console.log("Basename:", path.basename("/foo/bar/baz/asdf/index.html"))
