7c9e22da3bc57c5ae237d09d46ac08bfa8ae8a9c
Uttam <uttamrohit4545@gmail.com>
2025-07-25 19:47:45 +0600

    feat: add asynchronous file writing with writeFile

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 import {readFile} from 'node:fs';
+import { writeFile } from 'node:fs';
 
 readFile('./files/example.txt' , 'utf-8' ,(err,data) => {
     if(err) throw err;
     console.log(data);
 })
+
+
+//writeFile
+const content = "Hello uttam Sharma";
+writeFile('./files/message.txt' ,content , 'utf-8' , (err) => {
+    if(err) throw err;
+    console.log("File Created done")
+} )
