d0e729d27a6f84a455edeed18811d3e08fc7dee5
Uttam <uttamrohit4545@gmail.com>
2025-07-26 23:54:22 +0600

    feat: read large file using fs.createReadStream with stream events

---
+++ streams/index.js
@@ -0,0 +1,15 @@
+import fs, { createReadStream } from 'fs';
+
+const readableStream = createReadStream('files/large-file.txt',{
+    encoding:'utf-8',
+    highWaterMark : 1024
+})
+
+readableStream.on('data' ,(chunk) => {
+    console.log(`Recieved Chunk : ${chunk.length} bytes`)
+
+})
+
+readableStream.on('end' , () => {
+    console.log("Read All successfully");
+})
