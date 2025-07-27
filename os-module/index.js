f2e214d67003246d2a1d809eadb3096c7eb57119
Uttam <uttamrohit4545@gmail.com>
2025-07-27 23:34:54 +0600

    feat: compare traditional file reading with streaming approach

--- os-module/index.js
+++ os-module/index.js
@@ -3,4 +3,4 @@ console.log("cpu architecure : " ,os.arch());
 
 console.log("LOgical cpu core:", os.cpus())
 
-console.log("System Memory:(RAM) " , os.totalmem()/(1024 * 1024 * 1024) , "GB")
+console.log("Systems Memory:(RAM) " , os.totalmem()/(1024 * 1024 * 1024) , "GB")
