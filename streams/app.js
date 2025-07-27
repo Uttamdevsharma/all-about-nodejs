68165fbadebceb2c51651bd965aff1cda4195f28
Uttam <uttamrohit4545@gmail.com>
2025-07-27 23:33:48 +0600

    feat: compare traditional file reading with streaming approach

--- streams/app.js
+++ streams/app.js
@@ -1,7 +1,18 @@
-import fs from 'fs';
+import fs, { createReadStream } from 'fs';
 
 //tradionally read data FROM A File
 fs.readFile('files/large-file.txt','utf-8', (err,data) => {
     if(err) throw err;
-    console.log(data);
+    console.log(data.length + " bytes read");
+})
+
+//streaming to a file
+
+const readableStream = createReadStream('files/large-file.txt' , {
+    encoding:'utf-8',
+    highWaterMark:1024
+})
+
+readableStream.on('data' , (chunk) => {
+    console.log (`Processing : ${chunk.length} bytes`)
 })
