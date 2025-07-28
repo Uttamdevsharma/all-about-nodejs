3e44d328806b92ce20bdbd0e35ad3904bbc9ed2c
Uttam <uttamrohit4545@gmail.com>
2025-07-28 21:42:59 +0600

    feat: handle HTTP request methods in custom server

--- http-server/index.js
+++ http-server/index.js
@@ -1,13 +1,30 @@
+//again for req and res handle
+
 import http from 'http';
 
-const server = http.createServer( (req,res) => {
-    res.writeHead(200 , {"content-type" : "text/plain"})
-    res.end("Welcome to my server")
-    
+//create a server
+
+const server = http.createServer((req,res) => {
+    console.log("Request type:" , req.method)
+    //set headers
+    res.writeHead(200, {'content-type' : 'text/plain'});
+
+    //handle request based on request method
+    if(req.method === "GET"){
+        res.end("YOu made a get request")
+    } else if(req.method == "POST"){
+        res.end("YOu made POST request")
+    } else if(req.method === "PUT"){
+        res.end("YOU made put request")
+    }else {
+        // res.writeHead (404 ,{'content-type' : 'text/plain'});
+        res.end("404 - Not Found the request method")
+    }
 })
 
-const port = 3000;
+//start server
 
-server.listen(port , () => {
-    console.log(`The server is running ${port}`)
+const port = 3000;
+server.listen(port, () => {
+    console.log(`Server is running at port on ${port}`);
 })
