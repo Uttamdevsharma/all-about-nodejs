5ce7bfb29c270bd5f398efe5d5e7ac3fccf7da1e
Uttam <uttamrohit4545@gmail.com>
2025-07-29 19:19:26 +0600

    feat: create interactive CLI prompt using readline

--- my-custom-cli-tool/index.js
+++ my-custom-cli-tool/index.js
@@ -1,12 +1,29 @@
 #!/usr/bin/env node
 
 //command line arguments (process.argv)
-const args = process.argv.slice(2);
+// const args = process.argv.slice(2);
 
 
-if(args.length === 0){
-    console.log("Usage this command: custom-cli-codetutor1-project <name>")
-} else {
-    console.log(`Hello, ${args[0]}! Welcome to the CLI tool`)
-}
+// if(args.length === 0){
+//     console.log("Usage this command: custom-cli-codetutor1-project <name>")
+// } else {
+//     console.log(`Hello, ${args[0]}! Welcome to the CLI tool`)
+// }
+
+
+
+
+//Interactive cli using readline
+import readline from "readline";
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+//ask question to user
+rl.question("Whats your name ? \n", (name) =>{
+    console.log(`Hello, ${name}`);
+    rl.close();
+})
 
