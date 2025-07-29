4073700c2ae519ea8de04b35a72dce6388c89139
Uttam <uttamrohit4545@gmail.com>
2025-07-29 18:58:14 +0600

    chore: configure package.json for CLI tool entry point

--- my-custom-cli-tool/index.js
+++ my-custom-cli-tool/index.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
 
-console.log("Hello, This is your first CLI  tool.")
+//command line arguments (process.argv)
+const args = process.argv.slice(2);
+
+
+if(args.length === 0){
+    console.log("Usage this command: custom-cli-codetutor1-project <name>")
+} else {
+    console.log(`Hello, ${args[0]}! Welcome to the CLI tool`)
+}
+
