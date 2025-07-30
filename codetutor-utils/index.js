cf062153c2bc98c1aebf53b8c51805d70f74b126
Uttam <uttamrohit4545@gmail.com>
2025-07-30 15:50:10 +0600

    feat: export utility functions for CLI usage

---
+++ codetutor-utils/index.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+export { sum,multiply,capitalize } from "./utils.js";
+
---
+++ codetutor-utils/utils.js
@@ -0,0 +1,17 @@
+
+//sum the numbers
+export function sum (a,b) {
+    return a+b;
+
+}
+
+//multiply the number
+export const multiply = (a,b) => {
+    return a*b;
+}
+
+//capitalize string
+
+export const capitalize = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
