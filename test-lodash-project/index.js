4a0899d9a05741f80746231a4aab11e16a436545
Uttam <uttamrohit4545@gmail.com>
2025-07-30 21:36:02 +0600

    feat : test math functionality

--- test-lodash-project/index.js
+++ test-lodash-project/index.js
@@ -1,6 +1,13 @@
-const {chunk} = require('utilitex')
+const {chunk, add, multiply} = require('utilitex')
+
 
 const array = [1,2,3,4,5,6,7];
 
 const res = chunk(array,2);
 console.log(res);
+
+
+//math function
+console.log(add(2,3));
+
+console.log(multiply(40,20));
